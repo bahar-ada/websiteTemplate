5706d188cf9b24c10411ff6353230a6e15e103f5
b.adamiat <b.adamiat@irisaco.com>
2021-11-02 15:29:08 +0330

    second commit

---
+++ src/data/SliderData.js
@@ -0,0 +1,67 @@
+import one from '../images/one.jpg';
+import two from '../images/one.jpg';
+import three from '../images/one.jpg';
+import four from '../images/one.jpg';
+
+
+export const SliderData =[
+    {
+        title:'Villa',
+        price:'$23.000', 
+        path:'/home', 
+        label:'View Home', 
+        image:one,
+        alt:'House'
+},
+{
+    title:'Villa',
+    price:'$23.000', 
+    path:'/home', 
+    label:'View Home', 
+    image:two,
+    alt:'House'
+},
+{
+    title:'Villa',
+    price:'$23.000', 
+    path:'/home', 
+    label:'View Home', 
+    image:three,
+    alt:'House'
+},
+{
+    title:'Villa',
+    price:'$23.000', 
+    path:'/home', 
+    label:'View Home', 
+    image:four,
+    alt:'House'
+},
+];
+
+/* export const SliderDataTwo =[
+    {
+        title:'Villa',
+        price:'$23.000', 
+        path:'/home', 
+        label:'View Home', 
+        image:ImageFour,
+        alt:'House'
+},
+{
+    title:'Villa',
+    price:'$23.000', 
+    path:'/home', 
+    label:'View Home', 
+    image:ImageOne,
+    alt:'House'
+},
+{
+    title:'Villa',
+    price:'$23.000', 
+    path:'/home', 
+    label:'View Home', 
+    image:ImageOne,
+    alt:'House'
+},
+] */
