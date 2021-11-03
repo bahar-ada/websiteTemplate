cfad100e875676f0db035cd648336c98e0eb519e
b.adamiat <b.adamiat@irisaco.com>
2021-11-03 17:34:09 +0330

    infoSection commit

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Button } from './Button';
 
 const InfoSection = () => {
     return (
-        <div>
-            
-        </div>
+        <Section>
+            <Container>
+                <ColumnLeft>
+                    <h1>heading</h1>
+                    <p>paragraph</p>
+                    <p>paragraph</p>
+                    <Button to="/homes">
+                        label
+                    </Button>
+                </ColumnLeft>
+                <ColumnRight>
+                <img src="" alt="home"/>
+                </ColumnRight>
+            </Container>
+
+        </Section>
     )
 }
 
 export default InfoSection
+
+const Section = styled.section``
+const Container = styled.div``
+const ColumnLeft = styled.div``
+const ColumnRight = styled.div``
