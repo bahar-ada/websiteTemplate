6bfe7623109a8690b8722a3215099d1fe9f209d2
b.adamiat <b.adamiat@irisaco.com>
2021-11-07 15:25:52 +0330

    commit images

---
+++ src/data/InfoData.js
@@ -0,0 +1,24 @@
+
+import ImageOne from '../images/ImageOne.jpg';
+import ImageTwo from '../images/ImageTwo.jpg';
+
+export const InfoData ={
+    heading: 'Explore our beautiful homes',
+    paragraphOne:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    paragraphTwo:"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ",
+    buttonLabel:"View Home",
+    image: ImageOne,
+    reverse:false,
+    delay:100
+
+}
+
+export const InfoDataTwo ={
+    heading: 'Modern Designs',
+    paragraphOne:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    paragraphTwo:"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+    image: ImageTwo,
+    reverse:true,
+    delay:300
+
+}
